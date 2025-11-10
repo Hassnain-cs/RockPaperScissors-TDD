b2d89961a14dfeba107b30347a6f80e5ee8136a1
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2025-11-09 21:33:16 -0500

    RED: First failing test - Rock vs Scissors

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.cpp
@@ -1,16 +1,29 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+// Our game function - will determine who wins
+extern "C" {
+    char* determineWinner(const char player1[], const char player2[]) {
+        // Just return wrong for now so test fails
+        return "Wrong";
+    }
+}
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace RockPaperScissors
+namespace RockPaperScissorsTests
 {
-	TEST_CLASS(RockPaperScissors)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+    TEST_CLASS(RockPaperScissorsTests)
+    {
+    public:
+        // Test that Rock should beat Scissors
+        TEST_METHOD(TestPlayer1Wins_RockVsScissors)
+        {
+            // Call our function with Rock and Scissors
+            char* result = determineWinner("Rock", "Scissors");
+
+            // Check if it returns "Player1" - this will fail first
+            Assert::AreEqual("Player1", result);
+        }
+    };
 }
