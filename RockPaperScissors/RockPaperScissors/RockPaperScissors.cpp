d3dbe3d0a04994487c7667a45e6ff06073c9f124
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2025-11-09 21:30:53 -0500

    Initial Native Unit Test Project setup

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissors
+{
+	TEST_CLASS(RockPaperScissors)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
